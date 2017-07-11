7880bbd809ae19719393d1935d4324172937f368
zhuweiyou <i@zhuweiyou.com>
2017-07-11 19:02:23 +0800

    修复 dialog、alert、confirm 必须先 hide() 才能用的问题。

--- src/dialog/dialog.js
+++ src/dialog/dialog.js
@@ -1,13 +1,13 @@
 /*
 * Tencent is pleased to support the open source community by making WeUI.js available.
-* 
+*
 * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-* 
+*
 * Licensed under the MIT License (the "License"); you may not use this file except in compliance
 * with the License. You may obtain a copy of the License at
-* 
+*
 *       http://opensource.org/licenses/MIT
-* 
+*
 * Unless required by applicable law or agreed to in writing, software distributed under the License is
 * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 * either express or implied. See the License for the specific language governing permissions and
@@ -47,7 +47,7 @@ let _sington;
  *         onClick: function () { alert('确定') }
  *     }]
  * });
- * 
+ *
  * // 主动关闭
  * var $dialog = weui.dialog({...});
  * $dialog.hide(function(){
@@ -55,7 +55,7 @@ let _sington;
  * });
  */
 function dialog(options = {}) {
-    if(_sington) return _sington;
+    // if(_sington) return _sington;
 
     const isAndroid = $.os.android;
     options = $.extend({
@@ -82,7 +82,7 @@ function dialog(options = {}) {
             .addClass('weui-animate-fade-out')
             .on('animationend webkitAnimationEnd', function () {
                 $dialogWrap.remove();
-                _sington = false;
+                // _sington = false;
                 callback && callback();
             });
     }
